ace8219a547c8de8551454ee839394c0e6f1f478
えむけー <makoto_16n1200102@nnn.ed.jp>
2017-09-04 22:46:51 +0900

    会話ができるようにした

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'httpclient'
+
 class WebhookController < ApplicationController
   protect_from_forgery with: :null_session # CSRF対策無効化
 
@@ -12,7 +15,14 @@ class WebhookController < ApplicationController
 
     render plain: challenge if request.get?
 
-    render plain: request.body
+    httpclient = HTTPClient.new
+    httpclient.post_content(
+      'https://graph.facebook.com/v2.6/me/messages?access_token=EAABoiBhMihsBABXpnaNQZBg1zmsiFINZB4QVZCPR3qliiEuKxeU6Ksv8BRrQN1LrC6V2g9WbMTjJyqhoI6sZAWjemFOUGLZA4JnpzmZCsUbb7v4lVrBqMYS4mWbLH0RhKwNUmzeUfbJz4hehejrNpbNvB8YCJgHeltYX0j74qFBgZDZD',
+      {recipient: {id: '1764875153553086'}, message: {text: 'は？'}}.to_json,
+      'Content-Type' => 'application/json'
+    )
+
+    head :ok
   end
 
   private
