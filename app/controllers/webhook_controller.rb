181fc099dd77ad9729e24e84e6fd632e9006bbb0
えむけー <makoto_16n1200102@nnn.ed.jp>
2017-09-05 00:09:48 +0900

    淫夢語録をランダムに返すようにした

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -4,20 +4,21 @@ require 'json'
 require 'httpclient'
 
 class WebhookController < ApplicationController
-  protect_from_forgery with: :null_session # CSRF対策無効化
+  include WebhookHelper
 
+  protect_from_forgery with: :null_session # CSRF対策無効化
   TOKEN = ENV['FACEBOOK_TOKEN']
 
   def callback
 
-    params = JSON.parse(request.body.read, {:symbolize_names => true})
-    sender_id = params[:entry][0][:messaging][0][:sender][:id]
+    request_data = JSON.parse(request.body.read, {:symbolize_names => true})
+    sender_id = request_data[:entry][0][:messaging][0][:sender][:id]
     puts params
 
     httpclient = HTTPClient.new
     httpclient.post_content(
       'https://graph.facebook.com/v2.6/me/messages?access_token=EAABoiBhMihsBABXpnaNQZBg1zmsiFINZB4QVZCPR3qliiEuKxeU6Ksv8BRrQN1LrC6V2g9WbMTjJyqhoI6sZAWjemFOUGLZA4JnpzmZCsUbb7v4lVrBqMYS4mWbLH0RhKwNUmzeUfbJz4hehejrNpbNvB8YCJgHeltYX0j74qFBgZDZD',
-      {recipient: {id: sender_id}, message: {text: 'は？'}}.to_json,
+      {recipient: {id: sender_id}, message: {text: inmu}}.to_json,
       'Content-Type' => 'application/json'
     )
     head :ok
