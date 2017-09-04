95a343167d528279924062a6f5c2bfb6ae1fddaa
えむけー <makoto_16n1200102@nnn.ed.jp>
2017-09-04 23:06:13 +0900

    GETとPOSTでメソッドをわけた

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -9,17 +9,12 @@ class WebhookController < ApplicationController
   TOKEN = ENV['FACEBOOK_TOKEN']
 
   def callback
-    unless is_validate_token
-      return head :unauthorized
-    end
-
     httpclient = HTTPClient.new
     httpclient.post_content(
       'https://graph.facebook.com/v2.6/me/messages?access_token=EAABoiBhMihsBABXpnaNQZBg1zmsiFINZB4QVZCPR3qliiEuKxeU6Ksv8BRrQN1LrC6V2g9WbMTjJyqhoI6sZAWjemFOUGLZA4JnpzmZCsUbb7v4lVrBqMYS4mWbLH0RhKwNUmzeUfbJz4hehejrNpbNvB8YCJgHeltYX0j74qFBgZDZD',
       {recipient: {id: '1764875153553086'}, message: {text: 'は？'}}.to_json,
       'Content-Type' => 'application/json'
     )
-
     head :ok
   end
 
