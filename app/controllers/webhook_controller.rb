965b1e4da7a6fa3d1bb732cde45ede13daa370e9
えむけー <makoto_16n1200102@nnn.ed.jp>
2017-09-04 18:55:18 +0900

    Add webhook controller

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,20 @@
+class WebhookController < ApplicationController
+  protect_from_forgery with: :null_session # CSRF対策無効化
+
+  TOKEN = ENV['FACEBOOK_TOKEN']
+
+  def callback
+    unless is_validate_token
+      return head :unauthorized
+    end
+    challenge = params['hub.challenge']
+    logger.info({from_facebook: challenge})
+
+    head :ok
+  end
+
+  private
+  def is_validate_token
+    params['hub.mode'] == 'subscribe' && params['hub.verify_token'] == TOKEN
+  end
+end
